73cd3f074147c3ba33905ba434748b31c4802f28
James B Kemp <james@bibble.com>
2014-06-10 20:51:27 +0100

    menu display complete

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,19 +1,30 @@
 
 class Restaurant
 
-attr_accessor :menu
+	attr_accessor :menu
 
-def initialize
-	@menu ||= []
+	def initialize
+		@menu ||= []
+	end
+
+	def create_dish(name, price)
+		menu << {name: name, price: price}
+	end
+
+	def delete_dish(name)
+		menu.delete_if { |item| item[:name] == name }
+	end
+
+	def display_menu
+		print menu_contents
+	end
+
+	def menu_contents
+		menu.map { |dish| "#{dish[:name]} : £#{price_for(dish)}" }.join("\n")
+	end
+
+	def price_for(dish)
+		sprintf("%0.2f", dish[:price])
+	end
 end
 
-
-def create_dish(name, price)
-	menu << {name: name, price: price}
-
-end
-
-
-
-
-end
--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -6,10 +6,47 @@ describe Restaurant do
 
 	context 'has a menu of dishes' do
 
-		it "each dish has two key values, name and price" do
-		restaurant.create_dish('fish & chips', 5.00)
-		expect(restaurant.menu).to eq [{name: 'fish & chips', price: 5.00}]
+		it "has a menu with a dish with two key values, name and price" do
+		restaurant.create_dish('Fish & chips', 7.00)
+		expect(restaurant.menu).to eq [{name: 'Fish & chips', price: 7.00}]
 		end
+
+		it 'has a menu with multiple dises' do
+			restaurant.create_dish('Roast beef', 15.00)
+			restaurant.create_dish('Shepherds pie', 10.00)
+			restaurant.create_dish('Cornish pastie', 8.00)
+			expect(restaurant.menu).to eq [{name: 'Roast beef', price: 15.00}, {name: 'Shepherds pie', price: 10.00}, {name: 'Cornish pastie', price: 8.00}]
+		end
+
+		it 'can delete dishes from the menu' do
+			restaurant.create_dish('Roast beef', 15.00)
+			restaurant.create_dish('Shepherds pie', 10.00)
+			restaurant.delete_dish('Roast beef')
+			expect(restaurant.menu).to eq [{name: 'Shepherds pie', price: 10.00}]
+		end
+
+		it 'can print a menu' do
+			restaurant.create_dish('Roast beef', 15.00)
+			restaurant.create_dish('Shepherds pie', 10.00)
+			restaurant.create_dish('Cornish pastie', 8.00)
+			message = "Roast beef : £15.00\nShepherds pie : £10.00\nCornish pastie : £8.00"
+
+			expect(restaurant.menu_contents).to eq message
+
+		end
+
+		xit 'can receive an order from a customer' do
+		 customer = C
+		end
+
+		xit 'can add up the total price of an order' do
+		end
+
+		xit 'can tell a customer about an order' do
+		end
+
+
+
 
 	end
 end
